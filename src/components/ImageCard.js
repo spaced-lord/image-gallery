2fbe3ce97209423e2f5bbfbd2da4932a5a15b84a
spaced-lord <zackarynoe@gmail.com>
2021-04-23 12:55:32 -0600

    I just started building.

---
+++ src/components/ImageCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const ImageCard = ({ image }) => {
+    
+    return (
+     <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full" src ={image.webformatURL} alt=""></img>
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by {image.user}
+        </div>
+        <ul>
+          <li><strong>Views: </strong>{image.views}</li>
+          <li><strong>Downloads: </strong>{image.downloads}</li>
+          <li><strong>Likes: </strong>{image.likes}</li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-small font-semibold text-gray-700 mr-2">#tag1</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-small font-semibold text-gray-700 mr-2">#tag2</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-small font-semibold text-gray-700 mr-2">#tag3</span>
+      </div>
+    </div>
+    )
+}
+
+export default ImageCard;
+
